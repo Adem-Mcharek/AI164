fc89546989eef166bf17c849bab4c96e17019d0b
Adem-Mcharek <122184635+Adem-Mcharek@users.noreply.github.com>
2023-11-27 18:56:03 -0500

    db.js

--- models/db.js
+++ models/db.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql");
+const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  Host: 'database-1.ccmd6j6rmnku.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'password',
-  database: 'Adem_Mcharek',
-  port: '3306',
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
 });
 
-
+// Create a connection to the database without db.config
+// const connection = mysql.createConnection({
+//   Host: 'database-1.ccmd6j6rmnku.us-east-1.rds.amazonaws.com',
+//   user: 'admin',
+//   password: 'password',
+//   database: 'Adem_Mcharek',
+//   port: '3306',
+// });
 
 
 // open the MySQL connection
