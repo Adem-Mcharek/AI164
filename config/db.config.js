343ff8d8f320b56ce05af59f208c54f0d08e32f8
Adem-Mcharek <122184635+Adem-Mcharek@users.noreply.github.com>
2023-11-27 19:06:24 -0500

    db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     Host: 'database-1.ccmd6j6rmnku.us-east-1.rds.amazonaws.com',
     user: 'admin',
     password: 'password',
-    database: 'Adem_Mcharek',
+    DB: 'Adem_Mcharek',
 
     //port: '3306',
 };
